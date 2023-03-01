49f15fe59b28f482b7ba984fe70d91990dee6b6d
s.laptev <s.laptev@agatgroup.com>
2023-03-01 17:22:32 +0300

    test ci

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -4,7 +4,7 @@ import { mergeConfig } from 'vite'
 import { ViteFinal } from '@storybook/builder-vite'
 import svgLoader from 'vite-svg-loader'
 module.exports = {
-    stories: ['./../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
